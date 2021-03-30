45626804ab73bab018f316ec76ab296bfd36dc74
sunnextt <>
2021-03-31 00:45:33 +0100

    add address placeholder

--- app/src/index.js
+++ app/src/index.js
@@ -26,7 +26,7 @@ const config = {
   fieldsConfig:  [
    { id: 1, label: 'First Name', fieldName: 'firstName', type: 'text',placeholder:'Your First Name', isRequired: true , klassName:'first-name-field'},
    { id: 2, label: 'Last Name', fieldName: 'lastName', type: 'text', placeholder: 'Your Last Name', isRequired: true , klassName:'last-name-field'},
-   { id: 3, label: 'Email', fieldName: 'email', type: 'email', placeholder: ' Your Email', isRequired: true , klassName:'email-field'},
+   { id: 3, label: 'Email', fieldName: 'email', type: 'email', placeholder: ' Your Email Address', isRequired: true , klassName:'email-field'},
    { id: 4, label: 'Message', fieldName: 'msg', type: 'textarea',placeholder:'Write something.....', isRequired: true , klassName:'message-field'}
   ]
 }
